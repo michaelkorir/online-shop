2582766193370d70bcfbac5cb2b7d9d33a797c71
michael-korir <michaelkiptoo2000@gmail.com>
2023-11-23 11:02:15 +0300

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Online-Store/>
+      <h1>Online Shopping App</h1>
     </div>
   );
 }
--- src/Components/Online-Store/Online-Store.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Online_Store = () => {
-  return (
-    <div>Online-Store</div>
-  )
-}
