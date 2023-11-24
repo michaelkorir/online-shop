57e763ec757997cf3c08554a4279b3fddb8db0c0
michael-korir <michaelkiptoo2000@gmail.com>
2023-11-24 11:16:48 +0300

    handle events and imported components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import AddToCart from './components/AddToCart';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.escuelajs.co/api/v1/products');
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
       <h1>Online Shopping App</h1>
+      <div>
+        <SearchBar onSearch={handleSearch} />
+        <ProductList products={filteredProducts} onAddToCart={addToCart} />
+      </div>
+      <AddToCart products={products} setCart={setCart} />
+      <Cart cartItems={cart} removeFromCart={removeFromCart} />
     </div>
   );
 }
